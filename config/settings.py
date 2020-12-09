beeffad3bc3f75e710766ac9a71dcc6c58d6ce66
heasleykr@gmail.com <Partytime1@!>
2020-12-08 16:47:21 -0800

    added styling for FSDI 112 Assignment #3.

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap4',
     'posts'
 ]
 
@@ -120,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = [str(BASE_DIR.joinpath('static'))]
+
